fbc8b20f7ea2ce7db1b866a1b7d8efcff20322a4
Anna Krasilnikova <akr6363@mail.ru>
2023-02-04 19:55:16 +0300

    task 4

---
+++ 3.js
@@ -0,0 +1,21 @@
+function findOutlier(integers) {
+    let evenArr = []
+    let oddArr = []
+    for (let a of integers) {
+        a % 2 == 0 ?
+            evenArr.push(a) :
+            oddArr.push(a)
+
+    }
+    if (evenArr.length === 1) {
+
+        return evenArr[0]
+    } else {
+
+        return oddArr[0]
+    }
+
+}
+
+// findOutlier([160, 3, 1719, 19, 11, 13, -21])
+findOutlier([2, 4, 0, 100, 4, 11, 2602, 36])
