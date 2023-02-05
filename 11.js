bf036fd1662b28bcd903405ea9f50e9390ac2606
Anna Krasilnikova <akr6363@mail.ru>
2023-02-05 15:15:16 +0300

    task 11

---
+++ 11.js
@@ -0,0 +1,18 @@
+function generateHashtag(str) {
+    let str2 = str.replace(/\s+/g, '');
+    if (str2.length > 139 || str2.length < 1) {
+        console.log(false)
+        return false
+    } else {
+        console.log('#' + str.split(' ').filter(x => x !== '').map(el => el[0].toUpperCase() + el.slice(1)).join(''))
+        return '#' + str.split(' ').filter(x => x !== '').map(el => el[0].toUpperCase() + el.slice(1)).join('')
+    }
+
+}
+
+generateHashtag("Do We have A Hashtag")
+generateHashtag("    Hello     World   ")
+generateHashtag("     ")
+generateHashtag("a".repeat(139))
+generateHashtag("a".repeat(140))
+
